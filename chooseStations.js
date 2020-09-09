95b7946414bd0d57fcd3f1f01b92e4b668c53e2f
RAFH82 <hrechkar@gmail.com>
2020-09-09 23:32:00 +0000

    create a function to sort through an array for voting locations

---
+++ chooseStations.js
@@ -0,0 +1,20 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+const chooseStations = validStations => {
+  let results = [];
+  for (const station of validStations) {
+    if (station[1] >= 20) {
+      if (station[2] === 'school' || station[2] === 'community centre') {
+        results.push(station[0]);
+      } 
+    }
+  }
+ return results;
+}
+
+
+console.log(chooseStations(stations));
